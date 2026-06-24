ed295ec6a45f655f0d82f78e963a832219b31436
kethana jakkamputi <kethanajakkamputi@gmail.com>
2026-06-24 19:54:58 +0530

    Add Binary Numbers script to calculate max ones

---
+++ Binary Numbers.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    binary = bin(n)[2:]
+    
+    max_consecutive_ones = max(map(len, binary.split('0')))
+    
+    print(max_consecutive_ones)
